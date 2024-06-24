bc4434d5ce7fbca92862906ac2b49d35c99785c6
Jorge Eduardo Pérez Ramírez <supersparui@gmail.com>
2024-06-24 08:01:05 -0600

    Se agregó el archivo index del proyecto y se actualizó el archivo  readme.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # demoJorge
 Este repositorio nos servirá de práctica para la tecnología git.
+
+##Tecnologias
+Las tecnologias que se utilizan para este proyecto son PHP y mysql.
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina demo</title>
+</head>
+
+<body>
+    <h2>
+        Welcome to Jorge's page.
+    </h2>
+</body>
+
+</html>
